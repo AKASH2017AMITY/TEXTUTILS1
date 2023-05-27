8039795287bb96bcfd89bfc738ce84fbd22254ae
akash kumar <120650503+AKASH2017AMITY@users.noreply.github.com>
2023-05-27 14:46:07 +0530

    Updates

---
+++ precache-manifest.28e48d583c68b92ef786666f8ff9ceed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b072327fc21fe15284a",
+    "url": "/TEXTUTILS1/static/css/main.740c274d.chunk.css"
+  },
+  {
+    "revision": "4b072327fc21fe15284a",
+    "url": "/TEXTUTILS1/static/js/main.e63d6f52.chunk.js"
+  },
+  {
+    "revision": "8aac573ca158cfc7df43",
+    "url": "/TEXTUTILS1/static/js/runtime~main.8171aa7b.js"
+  },
+  {
+    "revision": "f3afda117f246c896c6c",
+    "url": "/TEXTUTILS1/static/js/2.ca48165b.chunk.js"
+  },
+  {
+    "revision": "ef276a8df7b31e3a5070",
+    "url": "/TEXTUTILS1/static/js/3.d62f7fe7.chunk.js"
+  },
+  {
+    "revision": "3da48e3b1f58ddc3e34ab7bf59e90b91",
+    "url": "/TEXTUTILS1/index.html"
+  }
+];
